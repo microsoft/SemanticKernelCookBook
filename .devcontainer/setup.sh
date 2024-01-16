ab3264988372e3690ac61903f7fe28bee12ec200
Kinfey <93169410+kinfey@users.noreply.github.com>
2024-01-16 12:11:20 +0000

    main

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ENV_NAME="pydev"
+
+PYTHON_VERSION="3.10.12"
+
+# Check if the environment already exists
+if (conda env list | grep $ENV_NAME); then
+    echo "$ENV_NAME environment already exists"
+else
+    echo "Creating $ENV_NAME environment"
+    conda create -n $ENV_NAME python=$PYTHON_VERSION -y
+fi
+
+source /opt/conda/etc/profile.d/conda.sh
+conda activate $ENV_NAME
