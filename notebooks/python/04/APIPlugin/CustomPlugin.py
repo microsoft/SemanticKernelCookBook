cf98833f7fd462baa1fc0fbbb6d984fbfe2ce0d2
kinfey <kinfeylo@microsoft.com>
2024-02-11 12:09:06 +0800

    update python sk 0.5.1.dev

---
+++ notebooks/python/04/APIPlugin/CustomPlugin.py
@@ -0,0 +1,29 @@
+# from semantic_kernel.skill_definition import sk_function, sk_function_context_parameter
+from semantic_kernel.plugin_definition import kernel_function,kernel_function_context_parameter
+from semantic_kernel import KernelContext
+
+
+class CustomPlugin:
+    @kernel_function(
+        description = "Get news from the web",
+        name = "NewsPlugin"
+    )
+    @kernel_function_context_parameter(name="location", description="location name")
+    def get_news_api(self, context: KernelContext) -> str:
+        return """Get news from the """ + context["location"] + """."""
+    
+    @kernel_function(
+            description="Search Weather in a city",
+            name="WeatherFunction"
+        )
+    @kernel_function_context_parameter(name="city", description="city string")
+    def ask_weather_function(self, context: KernelContext) -> str:
+        return context["city"] + "’s weather is 30 celsius degree , and very hot."
+    
+    @kernel_function(
+            description="Search Docs",
+            name="DocsFunction" 
+        )
+    @kernel_function_context_parameter(name="docs", description="docs string")
+    def ask_docs_function(self, context: KernelContext) -> str:
+        return "ask docs :" + context["docs"]
