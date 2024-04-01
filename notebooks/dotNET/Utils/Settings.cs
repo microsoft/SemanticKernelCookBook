f146ee9731bae00360a853402c0f2d9898e72c31
kinfey <kinfeylo@microsoft.com>
2024-04-01 22:01:51 +0800

    Update dotNET samples in Semantic Kernel .NET SDK 1.6.3

--- notebooks/dotNET/Utils/Settings.cs
+++ notebooks/dotNET/Utils/Settings.cs
@@ -1,6 +1,6 @@
 public class Settings
 {
-    public static string AOAIEndpoint = "Your Azure OpenAI Service Endpoint";
-    public static string AOAIKey = "Your Azure OpenAI Service API Key";
-    public static string AOAIModel = "Your Azure OpenAI Service gpt-35-turbo Deployment Name";
+    public static string AOAIEndpoint = "https://kinfey-aoai-demo.openai.azure.com/";
+    public static string AOAIKey = "8b32d28bc8d34fde9b63694170503fb6";
+    public static string AOAIModel = "GPT3Model";
 }
